77675b38e5731c55dd480f7d270ffb67fcf65cfa
hurayrahSam <kimjeyeon03@icloud.com>
2025-02-19 15:48:44 +0900

    fix: modify database in batch

--- apps/avto-shop-batch/src/database/database.module.ts
+++ apps/avto-shop-batch/src/database/database.module.ts
@@ -17,7 +17,7 @@ export class DatabaseModule {
     constructor(@InjectConnection() private readonly connection: Connection) {
         if (connection.readyState === 1) {
             console.log(
-                `MongoDB is connected into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} db`
+                `MongoDB is connected into ${process.env.ENV === 'production' ? 'production' : 'development'} db`
             );
         } else {
             console.log('DB is not connected!');
